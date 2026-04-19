f40db72c00cb531c93de98e36c5cd4d08be52b25
gustavo-apps <gustavo.gomes@appelsoft.com.br>
2026-04-19 18:38:42 -0300

    fix;

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -1,40 +1,35 @@
 #include <stdio.h>
 #include <math.h>
+
 int main(){
     int tamanho = 10;
-    int arraySelection[tamanho] = {1,5,2,3,4,9,7,6,8,10,0};    
-    int estaOrdenados = 0;
-    int menorNum = 0;
-
+    int arraySelection[tamanho] = {1,5,2,3,4,9,7,6,8,0};    
     for(int i = 0; i < tamanho; i++){
             printf("%d, ", arraySelection[i]);
     }
     printf("\n");
 
-    int trocaGeral = 0;
-    for (int i = 0; i < tamanho - 1; i++)
-    {
-        int posicao = i;
-        int menorNum = arraySelection[i];
-
-        for(int j = i + 1; j < tamanho; j++){
-            if(arraySelection[j] < menorNum){
-                menorNum = arraySelection[j];
-                posicao = j;
+    int troca = 0;
+            for (int i = 0; i < tamanho - 1; i++)
+            {
+                int indiceMenor = i;
+                for (int j = i + 1; j < tamanho; j++)
+                {
+                    if (arraySelection[j] < arraySelection[indiceMenor])
+                        indiceMenor = j;
+                }
+                if (indiceMenor != i)
+                {
+                    int temp = arraySelection[i];
+                    arraySelection[i] = arraySelection[indiceMenor];
+                    arraySelection[indiceMenor] = temp;
+                    troca++;
+                }
             }
-        }
-
-        if(posicao != i){
-            int aux = arraySelection[i];
-            arraySelection[i] = arraySelection[posicao];
-            arraySelection[posicao] = aux;
-            trocaGeral = 1;
-        }
-    }
 
     printf("\nOrdenacao\n");
     for(int i = 0; i < tamanho; i++){
         printf("%d, ", arraySelection[i]);
     }
     printf("\n");
-}
+}   
